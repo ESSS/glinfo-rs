5eed97f53febcaf7eb5698b1472c262aa96f9e7d
Bruno Oliveira <bruno@esss.com>
2025-06-30 15:29:09 -0300

    Move everything to a build function

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,28 @@ pub mod gl {
     pub use Gles2 as Gl;
 }
 
+pub fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = std::env::args().collect();
+    let args: Vec<&str> = args.iter().map(|s| s.as_str()).collect();
+    if let [_, "-h" | "--help"] = args[..] {
+        println!("Usage: glinfo [-f filename]");
+        return Ok(());
+    }
+
+    let output = match GLInfo::build() {
+        Ok(gl_info) => format!("{}", gl_info),
+        Err(err) => format!("ERROR: {}", err),
+    };
+
+    println!("{}", output);
+
+    if let [_, "-f", filename] = args[..] {
+        std::fs::write(filename, output.as_bytes())?;
+    }
+
+    Ok(())
+}
+
 #[derive(Debug, Clone)]
 enum GLDriver {
     LibGL,
@@ -63,86 +85,60 @@ impl Display for GLInfo {
 }
 
 impl GLInfo {
-    fn from_gl(driver: GLDriver, gl: &gl::Gl) -> Self {
-        GLInfo {
+    fn build() -> Result<Self, String> {
+        let event_loop =
+            EventLoop::new().map_err(|err| format!("Failed to create event loop: {err}"))?;
+        let template = ConfigTemplateBuilder::new();
+
+        let display_builder = DisplayBuilder::new()
+            .with_window_attributes(Some(Window::default_attributes().with_visible(false)));
+        // We just created the event loop, so initialize the display, pick the config, and
+        // create the context.
+        let (window, gl_config) =
+            match display_builder.build(&event_loop, template.clone(), |mut configs| {
+                configs.next().expect("Could not get any configs")
+            }) {
+                Ok((window, gl_config)) => (window.unwrap(), gl_config),
+                Err(err) => {
+                    return Err(err.to_string());
+                }
+            };
+
+        // Create gl context.
+        let (driver, context) = create_gl_context(&window, &gl_config)?;
+        let gl_context = Some(context.treat_as_possibly_current());
+
+        let attrs = window
+            .build_surface_attributes(Default::default())
+            .map_err(|err| format!("Failed to build surface attributes: {err}"))?;
+        let gl_surface = unsafe {
+            gl_config
+                .display()
+                .create_window_surface(&gl_config, &attrs)
+                .map_err(|err| format!("Failed to create a window surface: {err}"))?
+        };
+
+        // The context needs to be current for the Renderer to set up shaders and
+        // buffers. It also performs function loading, which needs a current context on
+        // WGL.
+        let gl_context = gl_context.as_ref().ok_or("Failed to get a GL context")?;
+        gl_context.make_current(&gl_surface).unwrap();
+
+        let gl_display = gl_config.display();
+
+        let gl = gl::Gl::load_with(|symbol| {
+            let symbol = CString::new(symbol).unwrap();
+            gl_display.get_proc_address(symbol.as_c_str()).cast()
+        });
+
+        Ok(GLInfo {
             driver,
             vendor: get_gl_string(&gl, gl::VENDOR),
             renderer: get_gl_string(&gl, gl::RENDERER),
             version: get_gl_string(&gl, gl::VERSION),
             shading_language: get_gl_string(&gl, gl::SHADING_LANGUAGE_VERSION),
-        }
+        })
     }
-}
-
-pub fn main() -> Result<(), Box<dyn Error>> {
-    let args: Vec<String> = std::env::args().collect();
-    let args: Vec<&str> = args.iter().map(|s| s.as_str()).collect();
-    if let [_, "-h" | "--help"] = args[..] {
-        println!("Usage: glinfo [-f filename]");
-        return Ok(());
-    }
-
-    let output = match get_gl_info() {
-        Ok(gl_info) => format!("{}", gl_info),
-        Err(err) => format!("ERROR: {}", err),
-    };
-
-    println!("{}", output);
-
-    if let [_, "-f", filename] = args[..] {
-        std::fs::write(filename, output.as_bytes())?;
-    }
-
-    Ok(())
-}
-
-fn get_gl_info() -> Result<GLInfo, String> {
-    let event_loop =
-        EventLoop::new().map_err(|err| format!("Failed to create event loop: {err}"))?;
-    let template = ConfigTemplateBuilder::new();
-
-    let display_builder = DisplayBuilder::new()
-        .with_window_attributes(Some(Window::default_attributes().with_visible(false)));
-    // We just created the event loop, so initialize the display, pick the config, and
-    // create the context.
-    let (window, gl_config) =
-        match display_builder.build(&event_loop, template.clone(), |mut configs| {
-            configs.next().expect("Could not get any configs")
-        }) {
-            Ok((window, gl_config)) => (window.unwrap(), gl_config),
-            Err(err) => {
-                return Err(err.to_string());
-            }
-        };
-
-    // Create gl context.
-    let (driver, context) = create_gl_context(&window, &gl_config)?;
-    let gl_context = Some(context.treat_as_possibly_current());
-
-    let attrs = window
-        .build_surface_attributes(Default::default())
-        .map_err(|err| format!("Failed to build surface attributes: {err}"))?;
-    let gl_surface = unsafe {
-        gl_config
-            .display()
-            .create_window_surface(&gl_config, &attrs)
-            .map_err(|err| format!("Failed to create a window surface: {err}"))?
-    };
-
-    // The context needs to be current for the Renderer to set up shaders and
-    // buffers. It also performs function loading, which needs a current context on
-    // WGL.
-    let gl_context = gl_context.as_ref().ok_or("Failed to get a GL context")?;
-    gl_context.make_current(&gl_surface).unwrap();
-
-    let gl_display = gl_config.display();
-
-    let gl = gl::Gl::load_with(|symbol| {
-        let symbol = CString::new(symbol).unwrap();
-        gl_display.get_proc_address(symbol.as_c_str()).cast()
-    });
-
-    Ok(GLInfo::from_gl(driver, &gl))
 }
 
 fn create_gl_context(
